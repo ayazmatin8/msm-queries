d95419fe5fc559488024b21335ed88737bc9ae57
ayazmatin <ayaz@chicagobooth.edu>
2021-05-05 17:43:40 +0000

    v3

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,6 +5,7 @@ class DirectorsController < ApplicationController
   end
 
   def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
     render({ :template => "director_templates/eldest.html.erb"})
   end
 end
